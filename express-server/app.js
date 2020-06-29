0510f40805e932708c0c40943af036c143d130c2
Doug Sewell <DouglasASewell@gmail.com>
2020-06-28 21:25:17 -0400

    Implemented requests, refactored code to utilize MVC model.

--- express-server/app.js
+++ express-server/app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const morgan = require('morgan'); //An NPM package for 3rd party middleware.
 const mongoose = require('mongoose');
-const Blog = require('./models/blog');
+const { render } = require('ejs');
+const blogRoutes = require('./routes/blogRoutes');
 
 //express app
 const app = express();
 
 //Connect to mongodb
-const dbURI = 'Database Information';
+const dbURI = 'ADD DATABASE INFORMATION';
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then((result) => app.listen(3000))
@@ -48,32 +49,6 @@ app.get('/about', (req, res) => {
     //res.send('<p>About page</p>'); //automatically sets header, infers status code as well
 });
 
-//Blog routes
-app.get('/blogs', (req,res) => {
-    Blog.find().sort({createdAt: -1}) //Soting by createdAt -1 will sort blog posts in order from newest to oldest.
-        .then((result) => {
-            res.render('index',{title:'All Blogs',blogs:result});
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-});
-
-app.post('/blogs',(req,res) => {
-    const blog = new Blog(req.body);
-    blog.save()
-        .then((result) => {
-            res.redirect('/blogs');
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-});
-
-app.get('/blogs/create', (req, res) => {
-    res.render('create', { title: 'Create a new blog' });
-});
-
 
 
 //Redirects
@@ -86,6 +61,10 @@ app.get('/blogs/create', (req, res) => {
 //The use always executes only if the request reaches this point in the code.
 //If a response is sent, code stops executing so this app.use method never fires.
 //Must be at the bottom of the code, otherwise it will fire before reaching other requests.
+
+//Blog routes
+app.use('/blogs', blogRoutes);
+
 app.use((req, res) => {
     res.status(404).render('404', { title: '404' });
     //res.status(404).sendFile('./views/404.html',{root:__dirname});
